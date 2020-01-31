b65990041209964b11a609d1bcbfd0a6e0470b8d
Troy Wiegand <troy@troywiegand.com>
2020-01-31 16:27:48 +0000

    Apply naming conventions everywhere

--- manifests/install_repo.pp
+++ manifests/install_repo.pp
@@ -8,12 +8,12 @@ class dotfiles::install_repo {
   }
 
   exec { "sudo git pull":
-    cwd    => '$dotfiles::repoPath',
-    onlyif => 'git -v && ls $dotfiles::repo_clone_path$dotfile::repo_dir_name',
+    cwd    => "${dotfiles::repo_clone_path}${dotfiles::repo_dir_name}",
+    onlyif => "git -v && ls ${dotfiles::repo_clone_path}${dotfile::repo_dir_name}",
   }
 
-  exec { "sudo git clone $dotfiles::repo_url $dotfiles::repo_dir_name":
-    cwd    => '$dotfiles::repo_clone_path',
+  exec { "sudo git clone ${dotfiles::repo_url} ${dotfiles::repo_dir_name}":
+    cwd    => $dotfiles::repo_clone_path,
     onlyif => 'git -v',
   }
 
