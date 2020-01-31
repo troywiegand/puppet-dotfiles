698c358eb8ee666c3393ff99fe3a4c3c8696a8df
Troy Wiegand <troy@troywiegand.com>
2020-01-31 18:06:58 +0000

    Change GH clone URL of dotfiles

--- manifests/clone_pull.pp
+++ manifests/clone_pull.pp
@@ -5,11 +5,11 @@
 class dotfiles::clone_pull {
   exec { "pull dotfiles":
     onlyif  => "/bin/bash test -d ${dotfiles::repo_clone_path}${dotfiles::repo_dir_name}",
-    command => "/bin/bash sudo git -C ${dotfiles::repo_clone_path}${dotfiles::repo_dir_name} pull",
+    command => "/bin/echo 'sudo git ${dotfiles::repo_clone_path}${dotfiles::repo_dir_name} pull' | /bin/bash",
   }
 
   exec { "clone dotfiles":
-    command => "/bin/bash sudo git clone ${dotfiles::repo_url} ${dotfiles::repo_dir_name}",
+    command => "/bin/echo 'sudo git clone ${dotfiles::repo_url} ${dotfiles::repo_dir_name}' | /bin/bash",
     cwd     => $dotfiles::repo_clone_path,
   }
 
