a0544c9d71b1bc34dbceaa91bf5dd7763887b3ec
Troy Wiegand <troy@troywiegand.com>
2020-01-31 19:19:24 +0000

    Implements creates attribute

--- manifests/clone_pull.pp
+++ manifests/clone_pull.pp
@@ -12,6 +12,7 @@ class dotfiles::clone_pull {
     command => "/bin/echo 'sudo git clone ${dotfiles::repo_url} ${dotfiles::repo_dir_name}' | /bin/bash",
     onlyif  => "/bin/bash test ! -d ${dotfiles::repo_clone_path}${dotfiles::repo_dir_name}",
     cwd     => $dotfiles::repo_clone_path,
+    creates => "${dotfiles::repo_clone_path}${dotfiles::repo_dir_name}",
   }
 
 }
